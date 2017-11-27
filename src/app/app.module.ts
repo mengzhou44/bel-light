fd3467344e3c6f1d5da7941ac9d501310b8ce730
meng zhou <mengzhou44@gmail.com>
2017-11-27 11:30:22 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { BLE } from '@ionic-native/ble';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, HomePage],
+  providers: [StatusBar, SplashScreen, BLE, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+
+const LIGHTBULB_SERVICE = 'ff10';
 
 @Component({
-  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  devices: any[] = [];
+  statusMessage: string;
+  constructor(
+    public navCtrl: NavController,
+    private ngZone: NgZone,
+    public toastCtrl: ToastController,
+    private ble: BLE
+  ) {}
+  ngOnInit() {
+    this.scan();
   }
 
+  scan() {
+    this.setStatus('Scanning for Bluetooth LE Devices');
+    this.devices = [];
+    this.ble
+      .scan([], 10)
+      .subscribe(
+        device => this.onDeviceDiscovered(device),
+        error => this.onError(error, 'Error scanning for Bluetooth low energy devices')
+      );
+
+    setTimeout(this.setStatus.bind(this), 10000, 'Scan complete!');
+  }
+
+  onDeviceDiscovered(device) {
+    this.ngZone.run(() => {
+      this.devices.push(device);
+    });
+  }
+
+  setStatus(message) {
+    this.statusMessage = message;
+  }
+
+  onError(error, message) {
+    this.setStatus('Error ' + error);
+    let toast = this.toastCtrl.create({
+      message,
+      position: 'middle',
+      duration: 5000
+    });
+    toast.present();
+  }
+
+  onDeviceSelected(device) {
+    console.log('A device is seleted.');
+  }
 }
